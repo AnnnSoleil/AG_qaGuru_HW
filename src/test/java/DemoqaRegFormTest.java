b8d5bc4f178d0684d3a6873e0b94fd6ec4de7a9f
Anna Gasnikova <however@bk.ru>
2026-02-24 00:38:19 +0300

    Add demoqaRegForm.java

---
+++ src/test/java/DemoqaRegFormTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DemoqaRegFormTest {
+    @Test
+    @Disabled
+    void successfulSearchTest() {
+        open("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=search]").shouldHave(text("https://selenide.org"));
+    }
+}
+
