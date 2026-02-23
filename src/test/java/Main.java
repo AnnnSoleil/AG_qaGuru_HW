b6121c2422109e0508e6d9ad865d0ef8ec88bc65
Anna Gasnikova <however@bk.ru>
2026-02-23 22:10:18 +0300

    Delete old search and add Main Hello world

---
+++ src/test/java/Main.java
@@ -0,0 +1,10 @@
+package guru.qa;
+
+public class Main {
+
+    private static String msg = "Hello world!";
+
+    public static void main(String... args) {
+        System.out.println(msg);
+    }
+}
