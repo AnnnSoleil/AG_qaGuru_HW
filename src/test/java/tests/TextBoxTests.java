b753f00419b9b7aff0674285cb869200e91d678f
Anna Gasnikova <however@bk.ru>
2026-03-09 22:33:07 +0300

    Simple testData at start of test

---
+++ src/test/java/tests/TextBoxTests.java
@@ -0,0 +1,45 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBoxTests extends TestBase {
+
+    @Test
+    void successfulFillFormTest() {
+        String userName = "Alex Black";
+        String userEmail = "alex@black.com";
+        String currentAddress = "first address 1";
+        String permanentAddress = "second address 2";
+
+        open("/text-box");
+        $("[id=userName]").setValue(userName);
+        $("[id=userEmail]").setValue(userEmail);
+        $("[id=currentAddress]").setValue(currentAddress);
+        $("[id=permanentAddress]").setValue(permanentAddress);
+        $("[id=submit]").click();
+
+        $("[id=output] [id=name]").shouldHave(text(userName));
+        $("[id=output] [id=email]").shouldHave(text(userEmail));
+        $("[id=output] [id=currentAddress]").shouldHave(text(currentAddress));
+        $("[id=output] [id=permanentAddress]").shouldHave(text(permanentAddress));
+        }
+
+
+    @Test
+    void successfulFillFormWithoutAddressTest() {
+        String userName = "Alex Black";
+        String userEmail = "alex@black.com";
+
+        open("/text-box");
+        $("[id=userName]").setValue(userName);
+        $("[id=userEmail]").setValue(userEmail);
+        $("[id=submit]").click();
+
+        $("[id=output] [id=name]").shouldHave(text(userName));
+        $("[id=output] [id=email]").shouldHave(text(userEmail));
+        }
+    }
