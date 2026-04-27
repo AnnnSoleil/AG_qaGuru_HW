873a0806c761c9915abaeb480f8f389a9fc4d4e3
Anna Gasnikova <however@bk.ru>
2026-04-27 21:29:07 +0300

    Added checks that file exist it archive for hw8 after 2 mentors comments

--- src/test/java/tests/ZipTests.java
+++ src/test/java/tests/ZipTests.java
@@ -21,29 +21,36 @@ public class ZipTests {
                 cl.getResourceAsStream("files/hw_qa_quru.zip")
         )) {
             ZipEntry entry;
+            boolean isPdfFound = false;
             while ((entry = zis.getNextEntry()) != null) {
                 if (entry.getName().endsWith(".pdf")) {
+                    isPdfFound = true;
                     byte[] pdfbytes = zis.readAllBytes();
                     PDF pdf = new PDF(pdfbytes);
                     assertThat(pdf.text).contains("антикризисный");
                 }
             }
+            assertThat(isPdfFound).isTrue();
         }
     }
+
     @Test
     void zipParsingXlsxTest() throws Exception {
         try (ZipInputStream zis = new ZipInputStream(
                 cl.getResourceAsStream("files/hw_qa_quru.zip")
         )) {
             ZipEntry entry;
+            boolean isXlsxFound = false;
             while ((entry = zis.getNextEntry()) != null) {
                 if (entry.getName().endsWith(".xlsx")) {
+                    isXlsxFound = true;
                     byte[] xlsxBytes = zis.readAllBytes();
                     XLS xls = new XLS(xlsxBytes);
                     String xlsxContent = xls.excel.getSheetAt(0).getRow(0).getCell(4).getStringCellValue();
                     assertTrue(xlsxContent.contains("Вторник"));
                 }
             }
+            assertThat(isXlsxFound).isTrue();
         }
     }
 
@@ -51,15 +58,18 @@ public class ZipTests {
     void zipParsingCsvTest() throws Exception {
         try (ZipInputStream zis = new ZipInputStream(
                 cl.getResourceAsStream("files/hw_qa_quru.zip")
-        )){
+        )) {
             ZipEntry entry;
-            while ((entry = zis.getNextEntry())!=null) {
+            boolean isCsvFound = false;
+            while ((entry = zis.getNextEntry()) != null) {
                 if (entry.getName().endsWith(".csv")) {
+                    isCsvFound = true;
                     byte[] csvBytes = zis.readAllBytes();
                     String csvContent = new String(csvBytes, StandardCharsets.UTF_8);
                     assertThat(csvContent).contains("Понедельник");
                 }
             }
+            assertThat(isCsvFound).isTrue();
         }
     }
 }
